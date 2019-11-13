3b5274a5a73aec2a813a86c00b2c21863191eee3
Victor Pierre <victor.pierre@student.ecp.fr>
2019-11-13 19:16:24 +0100

    backlog 2 done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 import Sound from 'react-sound';
 import Button from './Button';
 
-const apiToken = '<<Copiez le token de Spotify ici>>';
+const apiToken = 'BQDClZvrTNZWHRyn7K6LfWp_JVfyQ5qJch_t7yj1VD4AgPBD2nyNygi6eYI0oWL73TO5__8q_mxtxYomldfC6s-BQGrwcW5WA9FJcjCYEMZZ6VCyNXyEyulqcTWbERRdBV70QH4aUTb65LUI4eALIMrAUs2sSzJxKHLx5ESyviB_1g';
 
 function shuffleArray(array) {
   let counter = array.length;
@@ -28,26 +28,56 @@ function getRandomNumber(x) {
   return Math.floor(Math.random() * x);
 }
 
+
+
 class App extends Component {
 
   constructor() {
     super();
+    this.state = {
+      text: "",
+      songsLoaded: false,
+      songs:[],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ text: "Le composent est bien monté" })
+    fetch('https://api.spotify.com/v1/me/tracks', {
+      method: 'GET',
+      headers: {
+       Authorization: 'Bearer ' + apiToken,
+      },
+    })
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({ songsLoaded: true, tracks: data.items})
+        console.log("Réponse reçue ! Voilà ce que j'ai reçu : ", data);
+      })
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Bienvenue sur le Blindtest</h1>
-        </header>
-        <div className="App-images">
-          <p>Il va falloir modifier le code pour faire un vrai Blindtest !</p>
+    if (this.state.songsLoaded==false) {
+      return (
+        <img src={loading} className="App-logo" alt="logo"/>
+      )
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h1 className="App-title">Bienvenue sur le Blindtest</h1>
+          </header>
+          <div className="App-images">
+            <p>Il va falloir modifier le code pour faire un vrai Blindtest !</p>
+          </div>
+          <p>{this.state.tracks.length} chansons disponibles</p>
+          <p>Première chanson: {this.state.tracks[0].track.name}</p>
+          <div className="App-buttons">
+          </div>
         </div>
-        <div className="App-buttons">
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
